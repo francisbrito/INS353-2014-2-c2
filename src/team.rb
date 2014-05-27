245da10be57db2113df8d712c8549b114ad77682
Francis Brito <fr.br94@gmail.com>
2014-05-27 11:56:04 -0400

    Make  should add a player to the team test.

--- src/team.rb
+++ src/team.rb
@@ -1,8 +1,15 @@
 class Team
 	attr_accessor :name
+	attr_reader :player_count
 	def initialize(name)
 		@name = name
+		@player_count = 0
 	end
 	def add_player(name, position)
+		@player_count = @player_count + 1
+		@players ||= []
+		
+		p = Player.new name, position
+		@players.insert(position, p)
 	end
 end
